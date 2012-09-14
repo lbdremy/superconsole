8581a9c940efc249f7567a8d7ae60d8a6545bfc0
Remy Loubradou <remyloubradou@gmail.com>
2012-09-14 18:14:53 +0100

    Add moar options.
    - if options.timestamp sets to true, the ISO date is print (only if options.always=true || t || traced)
    - options.logLevel = "debug|info|...", will skip console calls under the logLevel given.
    - if options.printLevel sets to true, the log level is print in UPPERCASE.

--- console-trace.js
+++ console-trace.js
@@ -6,11 +6,15 @@
 var callsite = require('callsite')
   , tty = require('tty')
   , isatty = Boolean(tty.isatty() && process.stdout.getWindowSize)
-  , defaultColors = { log: '90', error: '91', warn: '93', info: '96', trace: '90' }
+  , defaultColors = { log: '90', error: '91', warn: '93', info: '96', trace: '90' , debug : '89' }
+  , severityLevels = { debug : 7, info : 6, log : 5, warn : 4, error : 3 , trace : 2}
+  , consoleMethods = ['debug' , 'info', 'log', 'warn', 'error', 'trace'];
 
 console.traceOptions = Object.create(null);
 console.traceOptions.cwd = process.cwd() + '/';
 console.traceOptions.colors = true;
+console.traceOptions.timestamp = false;
+console.traceOptions.printLevel = false;
 
 /**
  * Store custom options
@@ -23,29 +27,36 @@ module.exports = function (options) {
   if (options) {
     options.cwd = options.cwd || console.traceOptions.cwd;
     console.traceOptions = options;
-  }
+  } 
 }
+
 
 /**
  * Overrides the console methods.
  */
 
-;['error', 'log', 'info', 'warn', 'trace'].forEach(function (name) {
-  var fn = console[name];
-  console[name] = function () {
-    if (console._trace || console.traceOptions.always) {
-      if (Buffer.isBuffer(arguments[0])) {
-        arguments[0] = arguments[0].inspect()
-      } else if (typeof arguments[0] === 'object') {
-        arguments[0] = JSON.stringify(arguments[0], null, '  ');
+  consoleMethods.forEach(function (name) {
+    var fn = console[name];
+    // debug is not a native method
+    if(name == 'debug') fn = console.log;
+
+    console[name] = function () {
+      var dunotLog = console.traceOptions.logLevel 
+        && severityLevels[name] > severityLevels[console.traceOptions.logLevel];
+      if(dunotLog) return;
+      if (console._trace || console.traceOptions.always) {
+        if (Buffer.isBuffer(arguments[0])) {
+          arguments[0] = arguments[0].inspect()
+        } else if (typeof arguments[0] === 'object') {
+          arguments[0] = JSON.stringify(arguments[0], null, '  ');
+        }
+        var pad = (arguments[0] && !console.traceOptions.right || !isatty ? ' ' : '');
+        arguments[0] = console.traceFormat(__stack[1], name) + pad + arguments[0];
       }
-      var pad = (arguments[0] && !console.traceOptions.right || !isatty ? ' ' : '');
-      arguments[0] = console.traceFormat(__stack[1], name) + pad + arguments[0];
+      console._trace = false;
+      return fn.apply(this, arguments);
     }
-    console._trace = false;
-    return fn.apply(this, arguments);
-  }
-});
+  });
 
 /**
  * Overridable formatting function.
@@ -57,8 +68,15 @@ module.exports = function (options) {
 
 console.traceFormat = function (call, method) {
   var basename = call.getFileName().replace(console.traceOptions.cwd, '')
-    , str = '[' + basename + ':' + call.getLineNumber() + ']'
-    , color = '99'
+    , level = '[' + method.toUpperCase() + ']'
+    , timestamp = '[' +  new Date().toISOString() + ']'
+    , place = '[' + basename + ':' + call.getLineNumber() + ']'
+    , color = '99',
+    str = '';
+
+  str += console.traceOptions.printLevel ? level : '';
+  str += console.traceOptions.timestamp ? timestamp : '';
+  str += place;
 
   if (!isatty) {
     return str;
@@ -71,7 +89,6 @@ console.traceFormat = function (call, method) {
       color = console.traceOptions.colors[method];
     }
   }
-
   if (console.traceOptions.right) {
     var rowWidth = process.stdout.getWindowSize()[0];
     return '\033[s' + // save current position
