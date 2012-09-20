53f6278f08c002963656300e32d7effa6ec56aab
Remy Loubradou <remyloubradou@gmail.com>
2012-09-20 13:19:30 +0100

    NEW: Add tests for new options.

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
-require('./console-trace')
-require('./console-trace')({}) // should work even if I require it twice
+require('./superconsole')
+require('./superconsole')({}) // should work even if I require it twice
+
+var consoleMethods = ['debug' , 'info', 'log', 'warn', 'error']
 
 process.stdout.write('\n');
 
-;['error', 'log', 'info', 'warn'].forEach(function (name) {
+consoleMethods.forEach(function (name) {
   process.stdout.write(' ');
   console[name]('regular console.%s, no clue where it came from', name);
 });
 
 process.stdout.write('\n---------------------------------------------------------\n\n');
 
-;['error', 'log', 'info', 'warn'].forEach(function (name) {
+consoleMethods.forEach(function (name) {
   process.stdout.write(' ');
   console.traced[name]('this is a traced console.%s', name);
 });
@@ -19,7 +21,7 @@ process.stdout.write('\n--------------------------------------------------------
 
 console.traceOptions.colors = false;
 
-;['error', 'log', 'info', 'warn'].forEach(function (name) {
+consoleMethods.forEach(function (name) {
   process.stdout.write(' ');
   console.traced[name]('this is an uncolored traced console.%s', name);
 });
@@ -29,7 +31,7 @@ process.stdout.write('\n--------------------------------------------------------
 console.traceOptions.right = true;
 console.traceOptions.colors = true;
 
-;['error', 'log', 'info', 'warn'].forEach(function (name) {
+consoleMethods.forEach(function (name) {
   process.stdout.write(' ');
   console.traced[name]('this is a colored right aligned traced console.%s', name);
 });
@@ -39,24 +41,23 @@ process.stdout.write('\n--------------------------------------------------------
 console.traceOptions.always = true;
 console.traceOptions.colors = false;
 
-;['error', 'log', 'info', 'warn'].forEach(function (name) {
+consoleMethods.forEach(function (name) {
   process.stdout.write(' ');
   console[name]('this is an uncolored right aligned traced console.%s', name);
 });
 
 process.stdout.write('\n---------------------------------------------------------\n\n');
 
-require('./console-trace')({
+require('./superconsole')({
   colors: {
     log: '35',
     warn: '35',
     error: '35',
-    trace: '35',
     info: '35'
   }
 })
 
-;['error', 'log', 'info', 'warn', 'trace'].forEach(function (name) {
+consoleMethods.forEach(function (name) {
   process.stdout.write(' ');
   console.traced[name]('this is a magenta traced console.' + name);
 });
@@ -73,3 +74,39 @@ console.traced.log(['Works', 'with', 'Array']);
 
 process.stdout.write(' ');
 console.traced.log('Works with Buffer', Buffer('FooBar'));
+
+process.stdout.write(' ');
+console.traced.error(new Error('Works with error object and print the stack'));
+
+require('./superconsole')({
+  callsite : true,
+  level : true,
+  timestamp : true
+});
+
+consoleMethods.forEach(function (name) {
+  process.stdout.write(' ');
+  console.traced[name]('this is a colored traced console.%s with timestamp, log level and callsite info', name);
+});
+
+require('./superconsole')({
+  callsite : false,
+  level : false,
+  timestamp : true
+});
+
+consoleMethods.forEach(function (name) {
+  process.stdout.write(' ');
+  console[name]('this is a colored traced console.%s with timestamp info only', name);
+});
+
+require('./superconsole')({
+  callsite : false,
+  level : true,
+  timestamp : false
+});
+
+consoleMethods.forEach(function (name) {
+  process.stdout.write(' ');
+  console[name]('this is a colored traced console.%s with log level info only', name);
+});
