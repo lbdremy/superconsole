f618128be2ea121b03faf192cbfc463095f325bb
Jeremie Rodriguez <contact@jeremierodriguez.com>
2019-07-10 16:12:07 +0200

    feat: add JSON-formatted logging capability
    * feat: add JSON-formatted logging capability
    
    * fix: remove useless while when aggregating messages

--- test.js
+++ test.js
@@ -1,38 +1,19 @@
-require('./superconsole')
-require('./superconsole')({}) // should work even if I require it twice
+require('./superconsole');
+require('./superconsole')({}); // should work even if required twice
 
-var consoleMethods = ['debug' , 'info', 'log', 'warn', 'error']
+var consoleMethods = ['debug', 'info', 'log', 'warn', 'error'];
 
 process.stdout.write('\n');
 
-consoleMethods.forEach(function (name) {
-  process.stdout.write(' ');
+consoleMethods.forEach(function(name) {
   console[name]('regular console.%s, no clue where it came from', name);
 });
 
 process.stdout.write('\n---------------------------------------------------------\n\n');
 
-consoleMethods.forEach(function (name) {
-  process.stdout.write(' ');
-  console.traced[name]('this is a traced console.%s', name);
-});
-
-process.stdout.write('\n---------------------------------------------------------\n\n');
-
-console.traceOptions.colors = false;
-
-consoleMethods.forEach(function (name) {
-  process.stdout.write(' ');
-  console.traced[name]('this is an uncolored traced console.%s', name);
-});
-
-process.stdout.write('\n---------------------------------------------------------\n\n');
-
-console.traceOptions.right = true;
 console.traceOptions.colors = true;
 
-consoleMethods.forEach(function (name) {
-  process.stdout.write(' ');
+consoleMethods.forEach(function(name) {
   console.traced[name]('this is a colored right aligned traced console.%s', name);
 });
 
@@ -41,72 +22,57 @@ process.stdout.write('\n--------------------------------------------------------
 console.traceOptions.always = true;
 console.traceOptions.colors = false;
 
-consoleMethods.forEach(function (name) {
-  process.stdout.write(' ');
+consoleMethods.forEach(function(name) {
   console[name]('this is an uncolored right aligned traced console.%s', name);
 });
 
 process.stdout.write('\n---------------------------------------------------------\n\n');
+console.traced.log({ 1: 'works', 2: 'with', 3: 'Object' });
+console.traced.log(['Works', 'with', 'Array']);
+console.traced.log('Works with Buffer', Buffer.from('FooBar'));
+console.traced.error(new Error('Works with error object and print the stack'));
 
 require('./superconsole')({
-  colors: {
-    log: '35',
-    warn: '35',
-    error: '35',
-    info: '35'
-  }
-})
+  callsite: true,
+  level: true,
+  timestamp: true
+});
 
-consoleMethods.forEach(function (name) {
-  process.stdout.write(' ');
-  console.traced[name]('this is a magenta traced console.' + name);
+consoleMethods.forEach(function(name) {
+  console.traced[name]('this is a console.%s with timestamp, log level and callsite info', name);
+});
+
+require('./superconsole')({
+  callsite: false,
+  level: false,
+  timestamp: true
+});
+
+consoleMethods.forEach(function(name) {
+  console[name]('this is a console.%s with timestamp info only', name);
+});
+
+require('./superconsole')({
+  callsite: false,
+  level: true,
+  timestamp: false
+});
+
+consoleMethods.forEach(function(name) {
+  console[name]('this is a console.%s with log level info only', name);
 });
 
 process.stdout.write('\n---------------------------------------------------------\n\n');
 
-console.traceOptions.colors = true;
+require('./superconsole')({
+  callsite: true,
+  json: true,
+  level: true,
+  timestamp: true
+});
 
-process.stdout.write(' ');
-console.traced.log({ 1: 'works', 2: 'with', 3: 'Object' });
+consoleMethods.forEach(function(name) {
+  console[name]('this is a JSON-formatted console.%s log', name);
+});
 
-process.stdout.write(' ');
-console.traced.log(['Works', 'with', 'Array']);
-
-process.stdout.write(' ');
-console.traced.log('Works with Buffer', Buffer('FooBar'));
-
-process.stdout.write(' ');
 console.traced.error(new Error('Works with error object and print the stack'));
-
-require('./superconsole')({
-  callsite : true,
-  level : true,
-  timestamp : true
-});
-
-consoleMethods.forEach(function (name) {
-  process.stdout.write(' ');
-  console.traced[name]('this is a colored traced console.%s with timestamp, log level and callsite info', name);
-});
-
-require('./superconsole')({
-  callsite : false,
-  level : false,
-  timestamp : true
-});
-
-consoleMethods.forEach(function (name) {
-  process.stdout.write(' ');
-  console[name]('this is a colored traced console.%s with timestamp info only', name);
-});
-
-require('./superconsole')({
-  callsite : false,
-  level : true,
-  timestamp : false
-});
-
-consoleMethods.forEach(function (name) {
-  process.stdout.write(' ');
-  console[name]('this is a colored traced console.%s with log level info only', name);
-});
